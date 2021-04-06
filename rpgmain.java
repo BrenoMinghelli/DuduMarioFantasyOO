feb66be07474177a7637d7896b85423a0e873ce6
Brendan Buckley <brbuckley@id.uff.br>
2021-04-06 05:37:23 -0300

    Comentarios

--- rpgmain.java
+++ rpgmain.java
@@ -1,3 +1,30 @@
+/* Fafnir
+                Ideias:
+
+        Base:
+
+    - 4 herois e ate 4 inimigos
+    - Classe static combate tratando de cada batalha 4x4 com metodos como ataque(), item(), magia().
+    - Herois passados como parametro para reter modificacoes, mas inimigos instanciados dentro do metodo para que sejam temporarios.
+    - Fases pre-made tambem static herdando de combate.
+        
+        Mecanicas: 
+(ideias para incrementar o jogo depois que a base estiver 100%, tentei pensar em classes que teriam herança ou algo da materia)
+        
+    - Itens equipaveis em herois que modifiquem seus status
+    - Acontecimentos durante combate, como "ta chovendo, fogo -50% dano"
+    - Classe Boss 4x1 mas que nao herda de combate, com mecanicas unicas a cada boss.
+    - Elementos e tipo de dano. Fraqueza, resistencia, combinacoes.
+    
+(extra, nao tem mt a ver com a materia mas agregaria ao jogo)
+    - Historinha em texto antes do combate, "vcs estao andando pela floresta a caminho da cidade e sao surpreendidos por slimes"
+    
+                Comentarios:
+    
+    - tentar manter o minimo de coisas instanciadas na main, criar classes static.
+    - Talvez manter um contador de turnos ou loops na main, pode ser util mais pra frente.
+    
+*/
 import java.util.Scanner;
 public class rpgmain {
     public static void main(String args[]){
