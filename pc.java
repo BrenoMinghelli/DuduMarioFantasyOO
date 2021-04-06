eadc64659a0888c3899f4cacc1325e15c61cbcc3
BrenoMinghelli <78573895+BrenoMinghelli@users.noreply.github.com>
2021-04-06 02:42:58 -0300

    Add files via upload

---
+++ pc.java
@@ -0,0 +1,8 @@
+abstract class pc{ //Base dos personagens jogaveis
+    protected String nome;
+    protected int lvl;
+    protected int hp;
+    protected int hpA;
+    protected int atk;
+    protected int def;
+}
