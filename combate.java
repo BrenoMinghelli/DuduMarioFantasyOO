82fa5a9dd03d102d02050d4dd9d209ff3cc96135
Minghelludo <78573895+BrenoMinghelli@users.noreply.github.com>
2021-04-10 19:21:47 -0300

    Delete combate.java

--- combate.java
+++
@@ -1,3 +0,0 @@
-abstract class combate { //para nao ficar repetindo dentro dos encontros especificos,
-    protected fighter pc1;  //qualquer variavel q for re usavel entre os combater,
-}                          //como os pcs, ficaria aqui
