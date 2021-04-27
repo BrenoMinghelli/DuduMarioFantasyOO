fb900298ac43ddbb9556fbfa5f1ed00029899343
Minghelludo <78573895+BrenoMinghelli@users.noreply.github.com>
2021-04-26 23:04:45 -0300

    Delete randomroll.java

--- randomroll.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class randomroll {
-    public static int danoroll(int max){
-        Random r = new Random();
-        int ro=r.nextInt(max);
-        return ro;
-    }
-}
